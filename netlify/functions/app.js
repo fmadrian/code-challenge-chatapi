639bd25700c9283f6f8ae82ac3ed8141e51d1935
Adrián <47431198+fmadrian@users.noreply.github.com>
2025-02-21 14:50:15 -0600

    OpenAI integration

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
-
+const openai = require('openai');
 
 const app = express();
 
@@ -13,12 +13,31 @@ app.use(express.urlencoded({ extended: false }));
 router = express.Router();
 
 
-router.post('/completions', (req, res, next) => {
+router.post('/completions', async (req, res, next) => {
     try {
-        res.status(200).send({ message: 'ping' });
+        // Get Pokémon's name from request.
+        const { name } = req.body;
+        const apiKey = process.env.OPENAI_APIKEY;
 
+        // Create client and send prompt.
+        openAIClient = new openai.OpenAI({ apiKey });
+        const prompt = `Tell me an interesting or a funny fact about the Pokémon ${name}. If ${name} is not a Pokémon, just tell me it isn't a Pokémon and therefore you can't give me additional data about it.`;
+        const completion = openAIClient.chat.completions.create({
+            model: "gpt-4o-mini",
+            messages: [
+                { "role": "user", "content": prompt },
+            ],
+        });
+
+        // 3. Return result.
+        const fact = completion.then((result) => {
+            return result.choices[0].message.content ?? 'No response'
+        });
+
+        res.status(200).send({ code: 200, message: await fact });
     } catch (error) {
-        res.status(500).send({ message: 'Internal Chat API error.' });
+        console.error(error);
+        res.status(500).send({ message: `Internal OpenAI's API error.` });
     }
 })
 
