71dabf33e69700890174aa6b87cdae8f80826cf5
Adrián <47431198+fmadrian@users.noreply.github.com>
2025-02-22 18:51:35 -0600

    Refactored endpoint

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -40,25 +40,34 @@ router.post('/completions', async (req, res, next) => {
         const { name } = req.body;
         const apiKey = process.env.OPENAI_APIKEY;
 
-        // Create client and send prompt.
-        openAIClient = new openai.OpenAI({ apiKey });
-        const prompt = `Tell me an interesting or a funny fact about the Pokémon ${name}. If ${name} is not a Pokémon, just tell me it isn't a Pokémon and therefore you can't give me additional data about it.`;
-        const completion = openAIClient.chat.completions.create({
-            model: "gpt-4o-mini",
-            messages: [
-                { "role": "user", "content": prompt },
-            ],
-        });
+        // Check the name was provided to avoid sending a prompt with no Pokémon.
+        if (!name.trim()) {
+            return res.status(400).send({ code: 400, message: 'Please provide a Pokémon name.' });
+        }
 
-        // Return result.
-        const fact = completion.then((result) => {
-            return result.choices[0].message.content ?? 'No response'
-        });
+        // Create OpenAI client and send prompt.
+        let prompt = `Tell me an interesting or a funny fact about the Pokémon ${name}. If ${name} is not a Pokémon, just tell me it isn't a Pokémon and therefore you can't give me additional data about it.`;
+        let completion = null;
+        let fact = null;
+        try {
+            openAIClient = new openai.OpenAI({ apiKey });
+            completion = openAIClient.chat.completions.create({
+                model: "gpt-4o-mini",
+                messages: [
+                    { "role": "user", "content": prompt },
+                ],
+            });
 
-        res.status(200).send({ code: 200, message: await fact });
+            // Return result.
+            fact = completion.then((result) => {
+                return result.choices[0].message.content ?? 'Empty response.'
+            });
+            res.status(200).send({ code: 200, message: await fact });
+        } catch (error) {
+            res.status(500).send({ message: `Internal OpenAI's API error.` });
+        }
     } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: `Internal OpenAI's API error.` });
+        res.status(500).send({ code: 500, message: `Internal server error.` });
     }
 })
 
